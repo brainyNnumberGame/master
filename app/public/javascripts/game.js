f50cd6a69451f407086509161c879255484a1805
brainy <brainy168@live.com>
2015-09-21 23:07:24 +0800

    增加数字选择器界面

--- app/public/javascripts/game.js
+++ app/public/javascripts/game.js
@@ -11,13 +11,13 @@ var NoGameComponent = React.createClass({
 		return (
 			<div>
 				<GameNoBox no={this.state.no}/>
+				<ChooseGameNo/>
 			</div>
 		)
 	}
 });
-
+/*gameBox component*/
 var GameNoBox = React.createClass({
-	
 	render:function(){
 		var numbers = [];
 		this.props.no.forEach(function(no,index){
@@ -33,7 +33,7 @@ var GameNoBox = React.createClass({
 		);
 	}
 });
-
+/*ganme number component*/
 var GameNo = React.createClass({
 	getInitialState:function(){
 		return {
@@ -63,7 +63,7 @@ var GameNo = React.createClass({
 	},
 	render:function(){
 		return (
-			<span onClick={this.handleClick}>
+			<span onTouchStart={this.handleClick}>
 				<em className={this.state.styleClassFront} >
 					{this.props.no}
 				</em>
@@ -74,10 +74,76 @@ var GameNo = React.createClass({
 		);
 	}
 });
-var Game = React.render(
+/*choose number component*/
+var ChooseGameNo = React.createClass({
+	getInitialState:function(){
+		return {
+			no : [1,2,3,4,5,6,7,8,9,0]
+		}
+	},
+	render:function(){
+		return (
+			<div>
+				<ChooseGameNoBox no={this.state.no} />
+			</div>
+		)
+	}
+});
+/*choose number choosegamenobox*/
+var ChooseGameNoBox = React.createClass({
+	render:function(){
+		var numbers = [];
+		this.props.no.forEach(function(no,index){
+			numbers.push(
+				<ChooseGameNoDetail no={no}/>
+			);
+		});
+		return (
+			<div className='chooseNoBox' ref='chooseNoBox'>
+				{numbers}
+			</div>
+		)
+	}
+});
+var ChooseGameNoDetail = React.createClass({
+	getInitialState:function(){
+		return {
+			isCurrent : false 
+		}
+	},
+	handleClick:function(e){
+		var currentNo = e.target.innerHTML;
+		Array.from(e.target.parentNode.childNodes).forEach(function(dom,index){
+			dom.style.background = "#FFFFFF";
+		});
+		this.setState({
+			isCurrent : true 
+		})
+	},
+	render:function(){
+		var styleObj = {
+			background:this.state.isCurrent ? '#F923A3' : '#FFFFFF'
+		};
+		return (
+			<em style={styleObj} onTouchStart={this.handleClick}>
+				{this.props.no}
+			</em>
+		)
+	}
+});
+/*game render*/
+var game = React.render(
 	<NoGameComponent/>,
 	document.getElementById('gameCoreCon'),
 	function(){
 		console.log('done!');
 	}
 );
+/*choose game number render*/
+/*var chooseGameNo = React.render(
+	<ChooseGameNo/>,
+	document.getElementById('chooseGameNoCon'),
+	function(){
+		console.log('done!');
+	}
+);*/
